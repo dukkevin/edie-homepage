acf5d5f36ff315b1320235f4320ac01e30381513
Kevin Dumitrascu <dukkevin@gmail.com>
2021-11-28 23:44:59 +0200

    Navbar fix bug

--- animations/nav-scrolled.js
+++ animations/nav-scrolled.js
@@ -11,10 +11,3 @@ window.onscroll = function () {
         myNavLinks.classList.remove("minimize-links");
     }
 };
-
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarNavAltMarkup')
-const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
