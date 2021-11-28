fbc983ed2a8d2571913e33bc8c6ce19250264c93
Kevin Dumitrascu <dukkevin@gmail.com>
2021-11-28 18:21:19 +0200

    Navbar update

--- animations/nav-scrolled.js
+++ animations/nav-scrolled.js
@@ -11,3 +11,10 @@ window.onscroll = function () {
         myNavLinks.classList.remove("minimize-links");
     }
 };
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNavAltMarkup')
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
